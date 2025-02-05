02a760e87f737e6eb5d546f19bb3c19d465a35ca
whitefaalconn <ndzumaxx@gmail.com>
2025-02-05 00:53:11 +0000

    tryingggg

--- frontend/contexts/config.ts
+++ frontend/contexts/config.ts
@@ -1,7 +1,11 @@
 // This is a config file that loads in the config file
 const config = {
-  // get url from env
-  hostUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api",
-}
+  hostUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+    // Add this for debugging
+    getApiUrl: () => {
+      console.log('API URL:', process.env.NEXT_PUBLIC_API_URL);
+      return process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api';
+    }
+  };
 
 export default config
